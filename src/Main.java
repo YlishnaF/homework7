48f01b1c9dd4e0fd33d0b919381315f01c179f5a
geetbrains <ylishnaf@yandex.ru>
2020-04-28 21:09:47 +0500

    Дз про котиков сделана

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        Cat [] cats = new Cat[4];
+        cats[0] = new Cat ("Мурзик");
+        cats[1] = new Cat ("Пушистик");
+        cats[2] = new Cat ("Капитан мохнатые ботинки");
+        cats[3] = new Cat ("Мистр Котэ");
+
+        Plate plate = new Plate(100);
+
+        for (int i = 0; i < cats.length; i++) {
+            cats[i].eat(plate);
+            System.out.println("Аппетит котика на сегодня: " + cats[i].APPETITE);
+        }
+
+        plate.info();
+        plate.increaseFood();
+        plate.info();
+
+
+
 
 
     }
